5a8816b7154ed451ade3c788a6f5662f20a3edf2
hobaDevHome <heba96@yahoo.com>
2025-04-21 11:07:54 +0200

    adding web view

--- App.tsx
+++ App.tsx
@@ -1,17 +1,24 @@
-import React, {useState} from 'react';
-import {View, Button} from 'react-native';
-import ARScreen from './ARScreen'; // الشاشة اللي فيها AR view
+import React from 'react';
+import {useState} from 'react';
+import {View, Button, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {WebView} from 'react-native-webview';
 
 export default function App() {
   const [showAR, setShowAR] = useState(false);
+  console.log(WebView); // لو طلع undefined يبقى المشكلة في التثبيت
 
   return (
-    <View style={{flex: 1}}>
-      {!showAR ? (
-        <Button title="افتح الكاميرا" onPress={() => setShowAR(true)} />
-      ) : (
-        <ARScreen />
+    <SafeAreaView style={{flex: 1}}>
+      <Button title="افتح الكاميرا" onPress={() => setShowAR(!showAR)} />
+      {showAR && (
+        <WebView
+          source={{uri: 'https://lambent-pudding-89b56e.netlify.app/'}}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          originWhitelist={['*']}
+          allowsInlineMediaPlayback
+        />
       )}
-    </View>
+    </SafeAreaView>
   );
 }
